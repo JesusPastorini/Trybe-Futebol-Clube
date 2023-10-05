7ecffe9ce13c14763597b80cbf9af52098ccfe5e
JuniorPastorini <juniorpastorini_@hotmail.com>
2023-10-05 07:47:36 -0300

    req 18 update match

--- app/backend/src/routes/matches.router.ts
+++ app/backend/src/routes/matches.router.ts
@@ -2,10 +2,12 @@ import { Router } from 'express';
 import MatchesController from '../controllers/MatchesController';
 import validateToken from '../middleware/validateToken';
 import FinishMatchController from '../controllers/FinishMatchController';
+import UpdateMatchController from '../controllers/UpdateMatchController';
 
 const router = Router();
 
 router.get('/', MatchesController.getAllMatches);
 router.patch('/:id/finish', validateToken, FinishMatchController.finishMatch);
+router.patch('/:id', validateToken, UpdateMatchController.updateMatch);
 
 export default router;
