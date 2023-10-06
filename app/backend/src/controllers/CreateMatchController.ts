027b5fd29191d06f3cbc0070f9eae984bb20acf4
JuniorPastorini <juniorpastorini_@hotmail.com>
2023-10-06 00:19:25 -0300

    req 21

--- app/backend/src/controllers/CreateMatchController.ts
+++ app/backend/src/controllers/CreateMatchController.ts
@@ -1,24 +1,30 @@
 import { Request, Response } from 'express';
 import CreateMatchModels from '../models/CreateMatchModel';
+import Team from '../database/models/Team.models';
 
 class CreateMatchController {
   public static async createMatch(req: Request, res: Response): Promise<void> {
-    try {
-      const { homeTeamId, awayTeamId, homeTeamGoals, awayTeamGoals } = req.body;
-
-      const match = await CreateMatchModels.createMatch(
-        homeTeamId,
-        awayTeamId,
-        homeTeamGoals,
-        awayTeamGoals,
-      );
-
-      res.status(201).json(match);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: 'Internal server error' });
+    const { homeTeamId, awayTeamId, homeTeamGoals, awayTeamGoals } = req.body;
+    if (homeTeamId === awayTeamId) {
+      res.status(422)
+        .json({ message: 'It is not possible to create a match with two equal teams' });
+      return;
     }
+    const homeTeam = await Team.findByPk(homeTeamId);
+    const awayTeam = await Team.findByPk(awayTeamId);
+
+    if (!homeTeam || !awayTeam) {
+      res.status(404).json({ message: 'There is no team with such id!' });
+    }
+
+    const match = await CreateMatchModels.createMatch(
+      homeTeamId,
+      awayTeamId,
+      homeTeamGoals,
+      awayTeamGoals,
+    );
+
+    res.status(201).json(match);
   }
 }
-
 export default CreateMatchController;
