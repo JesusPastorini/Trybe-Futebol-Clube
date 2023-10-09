7234eb94fdc584dbc017fc0a398ada4c7b15483f
JuniorPastorini <juniorpastorini_@hotmail.com>
2023-10-09 18:59:48 -0300

    lint

--- app/backend/src/controllers/LeaderboardHomeController.ts
+++ app/backend/src/controllers/LeaderboardHomeController.ts
@@ -3,26 +3,25 @@ import LeaderboardService from '../services/LeaderboardService';
 
 class LeaderboardHomeController {
   public static async getHomeTeamLeaderboard(req: Request, res: Response): Promise<Response> {
-  
-      const data = await LeaderboardService.getHomeTeamLeaderboard();
-      data.sort((a, b) => {
-        // Primeiro, compare o total de pontos
-        if (b.totalPoints !== a.totalPoints) return b.totalPoints - a.totalPoints;
+    const data = await LeaderboardService.getHomeTeamLeaderboard();
+    data.sort((a, b) => {
+      // Primeiro, compare o total de pontos
+      if (b.totalPoints !== a.totalPoints) return b.totalPoints - a.totalPoints;
 
-        // Em caso de empate nos pontos, compare o saldo de gols
-        if (b.goalsBalance !== a.goalsBalance) {
-          return b.goalsBalance - a.goalsBalance;
-        }
+      // Em caso de empate nos pontos, compare o saldo de gols
+      if (b.goalsBalance !== a.goalsBalance) {
+        return b.goalsBalance - a.goalsBalance;
+      }
 
-        // Em caso de empate no saldo de gols, compare os gols marcados a favor
-        if (b.goalsFavor !== a.goalsFavor) {
-          return b.goalsFavor - a.goalsFavor;
-        }
+      // Em caso de empate no saldo de gols, compare os gols marcados a favor
+      if (b.goalsFavor !== a.goalsFavor) {
+        return b.goalsFavor - a.goalsFavor;
+      }
 
-        // Se ainda houver empate, compare o nome dos times (ordem alfabética)
-        return a.name.localeCompare(b.name);
-      });
-      return res.status(200).json(data);
+      // Se ainda houver empate, compare o nome dos times (ordem alfabética)
+      return a.name.localeCompare(b.name);
+    });
+    return res.status(200).json(data);
   }
 }
 export default LeaderboardHomeController;
